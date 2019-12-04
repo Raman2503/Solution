8c857659b72309ca829f8551022130e6e8edb558
Raman Khosravi <raman.khosravi@pace.de>
2019-12-04 10:09:46 +0100

    Removed faulty Unit Test "Sqrt.Test".

---
+++ SearchInsertPosition/Sqrt.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sqrt.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
