84d7629d83e3368c0b058b1c7e645c8afc99f967
Raman Khosravi <raman.khosravi@pace.de>
2019-12-06 17:09:12 +0100

    Added Unit Test for "ReverseInteger".

---
+++ SearchInsertPosition/ReverseInteger.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using ReverseInteger;
+using ReverseInteger.Test;
+
+namespace ReverseInteger.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Solution mySolution = new Solution();
+            int res = mySolution.Reverse(145);
+
+            Assert.AreEqual(res, 541);
+            
+        }
+    }
+}
