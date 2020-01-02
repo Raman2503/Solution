86fd1bc87a0c17134d451d171cb10eaf6435701b
Raman Khosravi <raman.khosravi@pace.de>
2020-01-02 17:46:06 +0100

    Added first Unit Test for method CalculateShareOfVendorStore.

---
+++ ConsignmentShopApp/ConsignmentShop/ConsignmentShopLibrary/ProfitsTest/Profits.Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using ConsignmentShopLibrary;
+
+namespace ProfitsTest
+{
+    [TestFixture]
+    public class ProfitsTest
+    {
+        [Test]
+        public void ClearedShoppingCartTest()
+        {
+            // Arrange 
+            Profits profits = new Profits();
+            Item item  = new Item();
+            Store store = new Store();
+
+            store.Vendors.Add(new Vendor { FirstName = "Bill", LastName = "Smith" });
+            store.Items.Add(new Item { Title = "Moby Dick", Description = "A book about a whale", Price = 4.50M, Owner = store.Vendors[0] });
+
+            profits.shoppingCartData.Add(new Item { Title = "Moby Dick", Description = "A book about a whale", Price = 4.50M, Owner = store.Vendors[0] });
+
+            //Act
+            var cartItems = profits.CalculateShareOfVendorStore();
+
+            // Assert
+            Assert.IsEmpty(cartItems);
+        }
+    }
+}
