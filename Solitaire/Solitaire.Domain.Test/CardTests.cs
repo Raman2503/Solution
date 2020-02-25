cbb63e21751f1cdb0a8227159e6b7309708989c1
nazariy.kalashnikov <nazar.kalashnikov@gmail.com>
2020-02-25 19:11:47 +0200

    Add an equality test for Card class

---
+++ Solitaire/Solitaire.Domain.Test/CardTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Solitaire.Domain.Test
+{
+	[TestFixture]
+	public class CardTests
+	{
+		[Test]
+		public void CreateCardWithRankAndSuite()
+		{
+			Card card1 = new Card(Rank.Jack,Suit.Clubs);
+
+			Card card2 = new Card(Rank.King, Suit.Hearts);
+		  
+			Console.WriteLine(card1.ToString());
+			Console.WriteLine(card2.ToString());
+		}
+
+		[Test]
+		public void CreateCardWithInvalidValues()
+		{
+			Card card1 = new Card(Rank.King,Suit.Diamonds);
+
+			Console.WriteLine(card1.ToString());
+		}
+
+
+		[Test]
+		public void CardEqualityTest()
+		{
+			Card card1 = new Card(Rank.King, Suit.Diamonds);
+			Card card2 = new Card(Rank.King, Suit.Diamonds);
+
+
+			Assert.AreEqual(card1, card2);
+
+			//uncomment next line to see that two cards are not same references
+			//Assert.AreSame(card1, card2);
+		}
+	}
+}
--- Solitaire/Solitaire.Domain/Card.cs
+++ Solitaire/Solitaire.Domain/Card.cs
@@ -6,20 +6,54 @@ using System.Threading.Tasks;
 
 namespace Solitaire.Domain
 {
-    public class Card
-    {
-        public Rank Rank { get; set; }
-        public Suit Suit { get; set; }
+	public class Card : IEquatable<Card>
+	{
+		public Rank Rank { get; set; }
+		public Suit Suit { get; set; }
 
-        public Card(Rank pRank, Suit pSuit)
-        {
-            Rank = pRank;
-            Suit = pSuit;
-        }
+		public Card(Rank pRank, Suit pSuit)
+		{
+			Rank = pRank;
+			Suit = pSuit;
+		}
 
-        public override string ToString()
-        {
-            return string.Format("{0} {1}",Rank,Suit);
-        }
-    }
+		public override string ToString()
+		{
+			return string.Format("{0} {1}",Rank,Suit);
+		}
+
+
+		public bool Equals(Card other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return Rank == other.Rank && Suit == other.Suit;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != this.GetType()) return false;
+			return Equals((Card)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return ((int)Rank * 397) ^ (int)Suit;
+			}
+		}
+
+		public static bool operator ==(Card left, Card right)
+		{
+			return Equals(left, right);
+		}
+
+		public static bool operator !=(Card left, Card right)
+		{
+			return !Equals(left, right);
+		}
+	}
 }
