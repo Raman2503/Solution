006a3809dc8a1d62c2e52f420857c78a5e608241
Raman Khosravi <raman.khosravi@txtgroup.com>
2020-03-13 17:58:46 +0100

    Started creating logic for game board.

---
+++ Solitaire/Solitaire.Domain.Test/BoardTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Solitaire.Domain.Test
+{
+	[TestFixture]
+	public class BoardTests
+	{
+		public IEnumerable<TestCaseData> MoveCardsTestCases
+		{
+			get
+			{
+				yield return new TestCaseData(
+					new List<Card>(),
+					new List<Card>(),
+					true
+				);
+
+				// Empty Case: Moving Card from empty pile to empty pile
+				// Move Card from normal pile to another pile
+			}
+		}
+
+		[Test]
+		[TestCaseSource(nameof(MoveCardsTestCases))]
+		public void MoveCardsTest(List<Card> pPile3, List<Card> pPile5, bool CardCanBeMoved)
+		{
+			//Arrange
+			Board board = new Board();
+
+			//Act
+			board.InitializeBoard(pPile3, pPile5);
+
+			board.MoveCardFromTableauToTableau(pPile3, pPile5);
+
+			//Assert
+			// Can this Card be moved ?
+			// If the Card can be moved, check if Card is correctly removed from pile and added to another pile
+			// If Card cannot be moved, ensure that Card is not removed from pile it was taken from
+			
+		}
+	}
+}
---
+++ Solitaire/Solitaire.Domain/Board.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitaire.Domain
+{
+	public class Board
+	{
+		public Tableau Tableau { get; set; }
+		public bool CardCanBeMoved { get; set; }
+
+		public void InitializeBoard(List<Card> pPile3, List<Card> pPile5)
+		{
+			
+		}
+
+		public bool MoveCardFromTableauToTableau(List<Card> pPile3, List<Card> pPile5)
+		{
+			return CardCanBeMoved;
+		}
+	}
+}
